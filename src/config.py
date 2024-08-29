a6ff4066dc90b98ee4bb3eb1829dc5886cc970b2
bladethazar <bladethazar@protonmail.com>
2024-08-29 22:36:42 +0200

    Removed get_mqtt_config()

--- src/config.py
+++ src/config.py
@@ -13,14 +13,3 @@ class PicoWConfig:
                     setattr(cls, key, value)
         except Exception as e:
             print(f"Error loading configuration: {e}")
-
-    @classmethod
-    def get_mqtt_config(cls):
-        """Return a dictionary of MQTT-specific configuration."""
-        return {
-            'broker_address': cls.MQTT_BROKER_ADDRESS,
-            'broker_port': cls.MQTT_BROKER_PORT,
-            'client_name': cls.MQTT_CLIENT_NAME,
-            'publish_interval': cls.MQTT_UPDATE_INTERVAL,
-            'reconnect_interval': cls.MQTT_UPDATE_INTERVAL,
-        }
